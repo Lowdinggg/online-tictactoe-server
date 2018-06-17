472ba65ea4536702b0d73ed1fbea2ecc8e874796
Lowdingg <lowdinggg@gmail.com>
2018-06-17 16:23:13 +0100

    Removed watermark

--- src/main/java/com/gitlab/morpion/Main.java
+++ src/main/java/com/gitlab/morpion/Main.java
@@ -2,11 +2,6 @@ package com.gitlab.morpion;
 
 import com.gitlab.morpion.network.NetworkManager;
 
-/**
- * Created by Lowdingg on 17/06/2018.
- *
- * @link http://lowding.ga/
- */
 public class Main {
 
     public static NetworkManager networkManager;
